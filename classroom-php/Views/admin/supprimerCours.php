5580b62d42ab72c9cf047442439d60ef1954bf43
sami <sami.elmejri@esprit.tn>
2022-09-09 03:48:27 +0100

    Update quiz

--- classroom-php/Views/admin/supprimerCours.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once '../../Controller/CoursC.php';
-
-$coursC = new CoursC();
-$coursC->supprimercours($_GET['id']);
-header('Location:tables.php');
-?>
--- classroom-php/Views/admin/supprimerQuiz.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once '../../Controller/QuizC.php';
-
-$quizC = new QuizC();
-$quizC->supprimerquiz($_GET['id']);
-header('Location:tables.php');
-?>
